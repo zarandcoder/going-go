62640f175a8b1ca215db4c833332146fc99526b4
Vadim Zar <23663743+zarandcoder@users.noreply.github.com>
2024-02-04 22:04:40 +0100

    Trigger pipeline test

--- run_hello.py
+++ run_hello.py
@@ -1,6 +1,11 @@
 name="Vadim"
 
 def introduce(ur_name):
+    a = 5
+    b = 10
+    print(f'a={a}; b={b}.....and now swap...')
+    a,b = b,a
+    print(f'a={a}; b={b}.....finish')
     print(f"Hello {ur_name}")
     print('How are you doing today?')
 
