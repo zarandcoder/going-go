645fcad24db15ff51f41600bc1213321f8957717
Vadim Zar <23663743+zarandcoder@users.noreply.github.com>
2024-02-04 23:11:07 +0100

    Trigger pipeline again

--- run_hello.py
+++ run_hello.py
@@ -1,12 +1,15 @@
 name="Vadim"
 
-def introduce(ur_name):
-    a = 5
-    b = 10
-    print(f'a={a}; b={b}.....and now swap...')
+def demo_swap(a=1, b=2):
+    print('Demo swap')
+    print(f'a={a}; b={b}....aand now magic is about to happen!')
     a,b = b,a
-    print(f'a={a}; b={b}.....finish')
+    print(f'Finish with magic...results are a={a}, b={b}')
+    
+
+def introduce(ur_name):
     print(f"Hello {ur_name}")
     print('How are you doing today?')
 
+demo_swap()
 introduce(name)
